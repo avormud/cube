6c79460d0feedf66e4f9b54c9045fbf63b710bf2
avormud <aleksandramuhomedyarova@gmail.com>
2026-05-27 21:31:30 +0500

    Add files via upload

---
+++ cube1.js
@@ -0,0 +1,36 @@
+
+const IMAGE_SRCS = [
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20624.png",
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20625.png",
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20626.png",
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20627.png",
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20628.png",
+
+  "https://githubraw.com/avormud/cube/refs/heads/main/Frame%20629.png"
+
+];
+
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+
+
+const FACE_NAMES = [
+
+  "Je gère",
+
+  "C’est la classe",
+
+  "On y va",
+
+  "Toujours chic",
+
+  "Sans stress",
+
+  "Tout est possible"
+
+];
+
